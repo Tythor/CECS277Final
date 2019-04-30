467b3d2f4a415de8be6702b69485c73df745ca69
Tythor <tthorin@live.com>
2019-04-29 22:45:01 -0700

    Update MakeReservation.java

--- src/FinalExam/MakeReservation.java
+++ src/FinalExam/MakeReservation.java
@@ -1,5 +1,32 @@
 package FinalExam;
 
 public class MakeReservation {
+    private Date date;
+    private Rooms room;
+    private Guest guest;
+    private boolean isFinalized;
 
+    public MakeReservation(Date date, Rooms room) {
+
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public Rooms getRoom() {
+        return room;
+    }
+
+    public void setRoom(Rooms room) {
+        this.room = room;
+    }
+
+    public void finalizeReservation() {
+
+    }
 }
