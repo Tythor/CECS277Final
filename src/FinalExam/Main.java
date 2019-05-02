a11d0ba4881620c5be641a62bcbdf30eaf497a60
Tythor <tthorin@live.com>
2019-05-02 13:01:04 -0700

    factory update

--- src/FinalExam/Main.java
+++ src/FinalExam/Main.java
@@ -2,6 +2,20 @@ package FinalExam;
 
 public class Main {
     public static void main(String[] args) {
+        SmallPartyRoom a = new SmallPartyRoom();
+        System.out.println(a);
 
+        MediumPartyRoom b = new MediumPartyRoom();
+        System.out.println(b);
+
+        AquaworldRoom c = new AquaworldRoom();
+        String d = c.newAquaworldRoom();
+        System.out.println(d);
+
+        BillardsLounge e = new BillardsLounge();
+        System.out.println(e);
+
+        KaraokeLounge f = new KaraokeLounge();
+        System.out.println(f);
     }
 }
