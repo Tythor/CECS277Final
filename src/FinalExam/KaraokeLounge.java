62df988d6021cf6b315755d34e81720d2ed39fad
Tythor <tthorin@live.com>
2019-05-02 11:43:05 -0700

    added factories

--- src/FinalExam/KaraokeLounge.java
+++ src/FinalExam/KaraokeLounge.java
@@ -1,10 +1,10 @@
 package FinalExam;
 
-public class KarokeLounge extends Room{
+public class KaraokeLounge extends Room{
 
 	
 	
-	public KarokeLounge() {
+	public KaraokeLounge() {
 		super.setCapacity(10);
 		super.setDescription("Enclosed lounge with karaoke machine. Includes access to karoke machine in cost.");
 		getCost();
@@ -26,7 +26,7 @@ public class KarokeLounge extends Room{
 		
 		
 public static void main(String[] args) {
-		KarokeLounge a = new KarokeLounge();
+		KaraokeLounge a = new KaraokeLounge();
 		System.out.println(a);
 	}
 
