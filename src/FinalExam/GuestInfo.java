00caac6608e7a9e80dc1cdf56c300addf506787d
Varun Krishna Pappusetti <vpkrishna99@gmail.com>
2019-05-03 12:12:43 -0700

    updated

--- src/FinalExam/GuestInfo.java
+++ src/FinalExam/GuestInfo.java
@@ -1,7 +1,111 @@
 package FinalExam;
+import java.sql.Time;
+import java.util.ArrayList;
+
+public class GuestInfo {
+    ArrayList<ManageReservation> manager_list;
+    private String name;
+    private String address;
+    private String phoneNum;
+    private String email;
+    private Card paymentMethod;
+    private Date date;
+    private Time time;
+    private MealPlans mealplan;
+    private Upgrades upgrade;
+
+    public GuestInfo(Date date, Room room) {
+        super(date, room);
+        // TODO Auto-generated constructor stub
+    }
+    public void attach(ManageReservation manager) {
+        manager_list.add(manager);
+    }
+    public void notify_manager() {
+
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 
 
-public class GuestInfo extends MakeReservation{
-    
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+
+    public void setPhoneNum(String phoneNum) {
+        this.phoneNum = phoneNum;
+    }
+
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPaymentMethod(String paymentInfo) {
+        this.paymentMethod = paymentMethod;
+    }
+
+    @Override
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public void setTime(Time time) {
+        this.time = time;
+    }
+
+    public void setMealplan(MealPlans mealplan) {
+        this.mealplan = mealplan;
+    }
+
+    public void setUpgrade(Upgrades upgrade) {
+        this.upgrade = upgrade;
+    }
+
+    // Getter Methods
+
+    public String getName() {
+        return name;
+    }
+
+
+    public String getAddress() {
+        return address;
+    }
+
+
+    public String getPhoneNum() {
+        return phoneNum;
+    }
+
+
+    public String getEmail() {
+        return email;
+    }
+
+
+    public String getPaymentMethod() {
+        return null;
+    }
+
+    @Override
+    public Date getDate() {
+        return date;
+    }
+
+    public Time getTime() {
+        return time;
+    }
+
+    public MealPlans getMealplan() {
+        return mealplan;
+    }
+
+    public Upgrades getUpgrade() {
+        return upgrade;
+    }
 }
 
