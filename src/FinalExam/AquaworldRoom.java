4daa075d67badc023ae549102f08bc826ccfb12f
Tythor <tthorin@live.com>
2019-05-06 22:59:47 -0700

    Daniel's Commits

--- src/FinalExam/AquaworldRoom.java
+++ src/FinalExam/AquaworldRoom.java
@@ -4,9 +4,20 @@ package FinalExam;
 public class AquaworldRoom extends Room{
 
 	private String restrictions;
+
 	private static AquaworldRoom aquaWorld = new AquaworldRoom();
 
+	/**
+	 * Default Constructor
+	 */
+
 	public AquaworldRoom() {}
+
+	/**
+	 *
+	 * @return - aquaWorld, the only instance of aquaWorld, representing the Singleton Pattern.
+	 * Singleton - This design pattern is a creational pattern that ensures that only one instance of the class can ever be made
+	 */
 
 	public static AquaworldRoom getInstance() {
 
@@ -19,7 +30,6 @@ public class AquaworldRoom extends Room{
 	}
 
 	public String newAquaworldRoom() {
-		super.name = "Aquaworld Room";
 		super.setCapacity(75);
 		super.setDescription("Olympic-sized pool with water slide, kiddie pool, and large jacuzzi. Includes access to showers/lockers, life guards on duty, DJ, table & chair set-up, and basic meal plan in cost.");
 		getRestrictions();
@@ -32,17 +42,17 @@ public class AquaworldRoom extends Room{
 	public String getRestrictions() {
 		restrictions = "To access water facilities, bathing suits must be worn at all times.";
 		return restrictions;
-		}
+	}
 
 	@Override
 	public double getCost() {
 		// TODO Auto-generated method stub
 		cost = 700;
 		return cost;
-		}
+	}
 
 	public String toString() {
-		return(name + ": \nCapacity: " + capacity + " people \nDescription: " + description + "\nRestrictions: " + restrictions + "\nCost: $" + cost +" an hour");
+		return("Aquaworld Room: \nCapacity: " + capacity + " people \nDescription: " + description + "\nRestrictions: " + restrictions + "\nCost: $" + cost +" an hour");
 	}
 }
 
--- src/FinalExam/PartyFavorsBag.java
+++ src/FinalExam/PartyFavorsBag.java
@@ -4,13 +4,28 @@ public class PartyFavorsBag extends Upgrades {
 
 	private int numPartyFavorsBag;
 
+
+	/**
+	 *
+	 * @param numPartyFavorsBag - the number of party favor bags being set an as integer.
+	 */
+
 	public void setNumPartyFavorsBag(int numPartyFavorsBag) {
 		this.numPartyFavorsBag = numPartyFavorsBag;
 	}
 
+	/**
+	 *
+	 * @return - the number of party favors bag that is being purchased per party as an integer
+	 */
+
 	public int getNumPartyFavorsBag() {
 		return numPartyFavorsBag;
 	}
+
+	/**
+	 * @return the total price of the party favors bag, which is 5 * the number of party favors bag
+	 */
 
 	public double getCost() {
 
