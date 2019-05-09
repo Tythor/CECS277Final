1e348127e82d3ce3753b9e1e0a7b7dcd670beda6
Varun Krishna Pappusetti <vpkrishna99@gmail.com>
2019-05-09 12:27:44 -0700

    Update Card.java

--- src/FinalExam/Card.java
+++ src/FinalExam/Card.java
@@ -1,41 +1,75 @@
 package FinalExam;
 
+/**
+ * This class represents a card that can be used as a payment
+ */
 public class Card {
-	
 	private String cardCompany;
 	private String ccNum;
 	private String securityCode;
 	private String expDate;
-	
+
+	/**
+	 * Getter for cardCompany
+	 * @return cardCompany
+	 */
 	public String getCardCompany() {
 		return cardCompany;
 	}
-	
-	public void setCardCompany(String cc) {
-		cardCompany = cc;
+
+	/**
+	 * Setter for cardCompany
+	 * @param cardCompany
+	 */
+	public void setCardCompany(String cardCompany) {
+		this.cardCompany = cardCompany;
 	}
-	
+
+	/**
+	 * Getter for ccNum
+	 * @return ccNum
+	 */
 	public String getCCNum() {
 		return ccNum;
 	}
-	
-	public void setCCNum(String num) {
-		ccNum = num;
+
+	/**
+	 * Setter for ccNum
+	 * @param ccNum
+	 */
+	public void setCCNum(String ccNum) {
+		this.ccNum = ccNum;
 	}
-	
+
+	/**
+	 * Getter for securityCode
+	 * @return securityCode
+	 */
 	public String getSecurityCode() {
 		return securityCode;
 	}
-	
-	public void setSecurityCode(String sc) {
-		securityCode = sc;
+
+	/**
+	 * Setter for securityCode
+	 * @param securityCode
+	 */
+	public void setSecurityCode(String securityCode) {
+		this.securityCode = securityCode;
 	}
-	
+
+	/**
+	 * Getter for expDate
+	 * @return expDate
+	 */
 	public String getExpDate() {
 		return expDate;
 	}
-	
-	public void setExpDate(String ed) {
-		expDate = ed;
+
+	/**
+	 * Setter for expDate
+	 * @param expDate
+	 */
+	public void setExpDate(String expDate) {
+		this.expDate = expDate;
 	}
 }
