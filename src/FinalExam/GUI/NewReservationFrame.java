2825677fb8dbcf3a3dc4beb17b36d15938b0c992
Surya C <suryatc787@gmail.com>
2019-05-15 23:50:08 -0700

    gmp+newreservationframe

--- src/FinalExam/GUI/NewReservationFrame.java
+++ src/FinalExam/GUI/NewReservationFrame.java
@@ -4,10 +4,15 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.*;
 
+import FinalExam.BasicMealPlan;
+import FinalExam.BronzeMealPlan;
 import FinalExam.Card;
+import FinalExam.GoldMealPlan;
 import FinalExam.Factory.*;
 import FinalExam.GuestInfo;
+import FinalExam.PlatinumMealPlan;
 import FinalExam.Room;
+import FinalExam.SilverMealPlan;
 
 public class NewReservationFrame implements ActionListener {
 
@@ -15,7 +20,7 @@ public class NewReservationFrame implements ActionListener {
 	private JLabel guestInfo, cardInfo, contact, cardType, fn, ln, pn, ad, db, em, cname, cnum, ccode, edate, roomDetail, roomNumber, date, time, mealPlanDetail;
 	private JCheckBox phoneBox, emailBox;
 	private JComboBox roomTypes, mealPlans, pizzaToppings, sodaChoices,  soda2, soda3, soda4, soda5,
-						toppings2, toppings3, toppings4, wingFlavors, wingFlavors2, iceCreamFlavors, iceCreamFlavors2, side, 
+						toppings2, toppings3, toppings4, wingType, wingFlavors, wingFlavors2, iceCreamFlavors, iceCreamFlavors2, side, 
 						cardCompanies;
 	private JButton save, cancel;
 
@@ -32,6 +37,7 @@ public class NewReservationFrame implements ActionListener {
 	private String[] pizzaToppingsList = {"Cheese", "Pepperoni", "Ham", "Jalapeno", "Sausage", "Mushroom", "Pineapple", "Bell Pepper",
 			"Onion", "Garlic Chicken"};
 	private String[] wingFlavorsList = {"Mild Spicy", "Diablo", "Lemmon Pepper", "BBQ", "Sesame"};
+	private String[] wingTypesList = {"Bone-In", "Boneless"};
 	private String[] iceCreamFlavorsList = {"Chocolate Fudge", "Vanilla Bean", "Strawberry Shortcake", "Choco-Mint", "Butter Pecan"};
 	private String[] sodaChoicesList = {"Coca-Cola", "Diet Coke", "Canada Dry", "Orange Crush", "Squirt", "Root Beer"};
 	private String[] sidesList = {"salad", "breadsticks"};
@@ -209,6 +215,7 @@ public class NewReservationFrame implements ActionListener {
 				soda3 = new JComboBox(sodaChoicesList);
 				soda4 = new JComboBox(sodaChoicesList);
 				soda5 = new JComboBox(sodaChoicesList);
+				wingType = new JComboBox(wingTypesList);
 				wingFlavors = new JComboBox(wingFlavorsList);
 				wingFlavors2 = new JComboBox(wingFlavorsList);
 				newPanel.add(toppings2);
@@ -217,6 +224,7 @@ public class NewReservationFrame implements ActionListener {
 				newPanel.add(soda3);
 				newPanel.add(soda4);
 				newPanel.add(soda5);
+				newPanel.add(wingType);
 				newPanel.add(wingFlavors);
 				newPanel.add(wingFlavors2);
 				newPanel.repaint();
@@ -231,6 +239,7 @@ public class NewReservationFrame implements ActionListener {
 				soda3 = new JComboBox(sodaChoicesList);
 				soda4 = new JComboBox(sodaChoicesList);
 				soda5 = new JComboBox(sodaChoicesList);
+				wingType = new JComboBox(wingTypesList);
 				wingFlavors = new JComboBox(wingFlavorsList);
 				wingFlavors2 = new JComboBox(wingFlavorsList);
 				iceCreamFlavors = new JComboBox(iceCreamFlavorsList);
@@ -241,6 +250,7 @@ public class NewReservationFrame implements ActionListener {
 				newPanel.add(soda3);
 				newPanel.add(soda4);
 				newPanel.add(soda5);
+				newPanel.add(wingType);
 				newPanel.add(wingFlavors);
 				newPanel.add(wingFlavors2);
 				newPanel.add(iceCreamFlavors);
@@ -290,8 +300,36 @@ public class NewReservationFrame implements ActionListener {
 					break;
 			}
 			Room room = roomFactory.createRoom();
-			System.out.println(room);
-        	
+			
+			String mp = mealPlans.getSelectedItem().toString();
+			switch(mp)
+			{
+			case "Basic Meal Plan":
+				BasicMealPlan bmp = new BasicMealPlan();
+				break;
+			case "Bronze Meal Plan":
+				BronzeMealPlan brmp = new BronzeMealPlan();
+				brmp.setSide(side.getSelectedItem().toString());
+				break;
+			case "Silver Meal Plan":
+				SilverMealPlan smp = new SilverMealPlan();
+				break;
+			case "Gold Meal Plan":
+				GoldMealPlan gmp = new GoldMealPlan();
+				gmp.setWingType(wingType.getSelectedItem().toString());
+				gmp.setWingFlavor1(wingFlavors.getSelectedItem().toString());
+				gmp.setWingFlavor2(wingFlavors2.getSelectedItem().toString());
+				break;
+			case "Platinum Meal Plan":
+				PlatinumMealPlan pmp = new PlatinumMealPlan();
+				pmp.setWingType(wingType.getSelectedItem().toString());
+				pmp.setWingFlavor1(wingFlavors.getSelectedItem().toString());
+				pmp.setWingFlavor2(wingFlavors2.getSelectedItem().toString());
+				pmp.setICFlavor1(iceCreamFlavors.getSelectedItem().toString());
+				pmp.setICFlavor2(iceCreamFlavors2.getSelectedItem().toString());
+				break;
+			}
+			
         }
     }
 
